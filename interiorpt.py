cef76dd0ba811735445eb7ec54f69be2e5877b57
Timur <t.usmanov@innopolis.university>
2024-10-22 16:11:05 +0300

    Fixing in progress

--- interiorpt.py
+++ interiorpt.py
@@ -17,7 +17,7 @@ def get_best(array, condition=lambda x: True):
 
 
 def normalize_row(mat, row, col):
-    mat[row] /= mat[row][col]
+    mat[row] /= mat[row, col]
 
 
 def add_row(mat, source_row, target_row, multiplier):
@@ -26,17 +26,17 @@ def add_row(mat, source_row, target_row, multiplier):
 
 def simplex(mat, eps):
     h, w = mat.shape
-    fracs = np.zeros(h - 1)
-    basic = np.arange(h - 1) + h - 1
+    fracs = [0] * (h - 1)
+    basic = [w - h + i for i in range(h)]
 
     while True:
-        col = get_best(mat[0], lambda x: x < -eps)
-        if col is None or mat[0][col] > -eps:
+        col = get_best(list(mat[0].A1), lambda x: x < -eps)
+        if col is None or mat[0, col] > -eps:
             break
 
         for i in range(h - 1):
-            sol = mat[i + 1][-1]
-            divisor = mat[i + 1][col]
+            sol = mat[i + 1, -1]
+            divisor = mat[i + 1, col]
             if sol >= 0 and sol <= eps and divisor < 0:
                 fracs[i] = -1
             else:
@@ -45,7 +45,7 @@ def simplex(mat, eps):
         row = get_best(fracs, lambda a: a >= 0 and a <= 1e9)
 
         if row is None or row == h - 1:
-            return UNSOLVABLE
+            return (UNSOLVABLE,)
 
         basic[row] = col
         row += 1
@@ -59,24 +59,26 @@ def simplex(mat, eps):
 
         solcell = mat[0][-1]
         if not np.isfinite(solcell) or solcell >= 1e9:
-            return UNBOUNDED
+            return (UNBOUNDED,)
 
-    return SOLVED
+    return SOLVED, mat, basic
 
 
-def run_simplex(objective, nconstraints, mat, rhs_values, eps):
-    input_vector = objective
-    obj = list(map(float, input_vector.split()))
-    nvars = len(obj)
+def run_simplex(objective, mat, rhs_values, eps):
+    obj = objective
+    nconstraints, nvars = mat.shape
+    _mat = mat
 
-    mat = np.zeros((1 + nconstraints, nvars + nconstraints + 1))
-    mat[0, :nvars] = -np.array(obj)
-
+    mat = np.matrix([[0] * (nvars + 1) for i in range(nconstraints + 1)])
+    for i in range(nvars):
+        mat[0, i] = -obj[i, 0]
+    for i in range(nconstraints):
+        for j in range(nvars):
+            mat[i + 1, j] = _mat[i, j]
     for i in range(1, nconstraints + 1):
-        mat[i, -1] = rhs_values[i - 1]
+        mat[i, -1] = rhs_values[i - 1, 0]
 
-    basic = []
-    solver_state = simplex(mat, eps)
+    solver_state, mat, basic = simplex(mat, eps)
 
     if solver_state != SOLVED:
         print("The method is not applicable!")
@@ -123,19 +125,11 @@ def interior_solve(objective, constraints, initialsol, alpha, eps):
 
 
 def find_max(objective, x):
-    n = len(objective)
-    ans = 0
-    for i in range(n):
-        ans += objective[i]*x[i]
-    return ans
+    return round((objective.T * x).A1[0], 3)
 
 
 def display(vertical_vec, n):
-    s = ''
-    for i in range(n):
-        s += vertical_vec[i, 0] + ' '
-    print(s)
-    return s
+    return tuple(round(i, 3) for i in vertical_vec.A1)
 
 
 def check_applicability(answer):
@@ -158,29 +152,28 @@ def main():
     in_constraints = ';'.join(input() for i in range(constraint_count))
 
     constraints = np.matrix(in_constraints)
-    nvars = constraints.shape[1]
 
     inp = input('Initial starting point: ')
     initialsol = np.matrix(inp).T
 
     inp = input('Vector of right-hand side numbers: ')
-    right_hand_side = ';'.join(input() for i in range(constraint_count))
+    right_hand_side = np.matrix(inp).T
 
-    eps = int(input())
+    eps = float(input("Epsilon: "))
     alpha05 = 0.5
     alpha09 = 0.9
 
     answer_alpha05 = interior_solve(objective, constraints, initialsol, alpha05, eps)
     if check_applicability(answer_alpha05):
-        print('Alpha = 0.5: x* =', display(answer_alpha05[1], n))
+        print('\nAlpha = 0.5: x* =', display(answer_alpha05[1], n))
         print('max:', find_max(objective, answer_alpha05[1]))
 
     answer_alpha09 = interior_solve(objective, constraints, initialsol, alpha09, eps)
     if check_applicability(answer_alpha09):
-        print('Alpha = 0.9: x* =', display(answer_alpha09[1], n))
+        print('\nAlpha = 0.9: x* =', display(answer_alpha09[1], n))
         print(find_max(objective, answer_alpha09[1]))
 
-    run_simplex(objective, nvars, constraints, right_hand_side, eps)
+    run_simplex(objective, constraints, right_hand_side, eps)
 
 
 if __name__ == "__main__":
